5c2e346b8f334b9eb211498a86689c4df9ecacca
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2022-05-09 16:55:59 +0200

    Update vl_assay_comparison.R

--- vl_assay_comparison.R
+++ vl_assay_comparison.R
@@ -140,10 +140,15 @@ ggplot(data = LAg_stradling_ART_start,
   ) +
   ylab('Sedia LAg ODn') + xlab('Time from ART start (Days)')
 
+model_values_stradling_ARTstart <- nlme::lme(fixed = logvl ~ hiv_antibody_value,
+                    random = ~ 1 | subject_label_blinded,
+                    data = LAg_stradling_ART_start, #%>%
+                      # filter(time_on_trt>=0),
+                    na.action = na.exclude, control = lmeControl(opt = "optim")
+)
+summary(model_values_stradling_ARTstart)
+
 # studying the trajectory using CEPHIA samples that don't have pre 
-
-assay_dataset <- read.csv("data/cephia_public_use_dataset_20210604.csv")
-
 sedia_generic <- read_csv("data/20180410-EP-LAgSedia-Generic.csv") %>%
   filter(visit_hivstatus == "P") %>%
   rename(sedia_ODn = result...72) %>%
@@ -157,72 +162,57 @@ data_generated <- sedia_generic %>%
   filter(!is.na(sedia_ODn)) %>%
   distinct(subject_label_blinded, test_date, .keep_all = T) %>%
   group_by(subject_label_blinded) %>%
+  mutate(inter_test_interval = c(30, diff(test_date))) %>%
+  filter(inter_test_interval>=30) %>% #removing visits that are less than two months apart. CEPHIA has some visits that are even one day apart
+  filter(!is.na(viral_load)) %>%
   mutate(visits = 1:length(subject_label_blinded)) %>%
   mutate(n_visits = max(visits)) %>%
   filter(n_visits>1) %>%
   ungroup() %>%
   mutate(time_on_trt = as.numeric(test_date - art_initiation_date),
          `Sedia LAg Odn screen` = sedia_ODn) %>%
-  dplyr::select(subject_label_blinded, test_date, art_initiation_date, time_on_trt, `Sedia LAg Odn screen`, viral_load, n_visits) %>%
-  arrange(subject_label_blinded, test_date) 
+  dplyr::select(subject_label_blinded, test_date, art_initiation_date, time_on_trt, 
+                `Sedia LAg Odn screen`, sedia_ODn, viral_load, n_visits) %>% #, inter_test_interval
+  arrange(subject_label_blinded, test_date)
 
-set.seed(11)
-  ggplot(data = data_generated %>%
-           filter(subject_label_blinded %in% c(11018648, 11428415)), #%in% sample(unique(subject_label_blinded), size = 30, replace = F)
-         aes(x = time_on_trt, y = `Sedia LAg Odn screen`)) + #as.factor(subject_label_blinded)
-    geom_line(aes(color = as.factor(subject_label_blinded)), size = 1.5) +#aes(color = subject_label_blinded, linetype = Group), group = Group
-    theme(
-      text = element_text(size = 20),
-      plot.title = element_text(hjust = 0.5),
-      axis.line = element_line(colour = "black"),
-      axis.text = element_text(size = 18),
-      axis.title = element_text(size = 18),
-      panel.background = element_blank(),
-      panel.border = element_blank(),
-      plot.margin=unit(c(0,0,0,0), "null"),
-      legend.position = "none"
-    ) +
-    ylab('Sedia LAg ODn') + xlab('Time from ART start (Days)')
-  
-# data_generated <- assay_dataset %>%
-#   filter(assay == "LAg-Sedia",
-#          assay_result_field == "final_result",
-#          cephia_panel == "CEPHIA 1 Evaluation Panel") %>%
-#   pivot_wider(names_from = assay_result_field, values_from = assay_result_value) %>%
-#   filter(
-#     # treatment_naive_at_visit == FALSE, 
-#     ever_designated_as_elite_controller == FALSE#,
-#     # days_since_eddi <= 1000,
-#     # (eddi_interval_size <= 120 | days_since_eddi >= 2 * eddi_interval_size)
-#   ) %>%
-#   mutate(eddi = days_since_eddi, assay_value = final_result, viral_load = viral_load_closest_to_visit,
-#          specimen = generic_result_identifier, subject_label_blinded = participant_identifier) %>%
-#   group_by(subject_label_blinded) %>%
-#   mutate(patients_ever_on_ART = max(treatment_naive_at_visit)) %>%
-#   select(specimen, subject_label_blinded, viral_load, eddi, assay_value) %>%
-#   select(subject_label_blinded, viral_load, eddi, ODn)
+data_generated_with_suppression <- sedia_generic %>%
+  select(subject_label_blinded, test_date, art_initiation_date, aids_diagnosis_date, 
+         art_interruption_date, art_resumption_date, treatment_naive, 
+         on_treatment, first_treatment, sedia_ODn, viral_load) %>%
+  filter(!is.na(art_initiation_date)) %>%
+  arrange(subject_label_blinded, test_date) %>%
+  filter(!is.na(sedia_ODn)) %>%
+  filter(viral_load <=400) %>%
+  distinct(subject_label_blinded, test_date, .keep_all = T) %>%
+  group_by(subject_label_blinded) %>%
+  mutate(inter_test_interval = c(30, diff(test_date))) %>%
+  filter(inter_test_interval>=30) %>% #removing visits that are less than two months apart. CEPHIA has some visits that are even one day apart
+  filter(!is.na(viral_load)) %>%
+  mutate(visits = 1:length(subject_label_blinded)) %>%
+  mutate(n_visits = max(visits)) %>%
+  filter(n_visits>1) %>%
+  ungroup() %>%
+  mutate(time_on_trt = as.numeric(test_date - art_initiation_date),
+         `Sedia LAg Odn screen` = sedia_ODn) %>%
+  dplyr::select(subject_label_blinded, test_date, art_initiation_date, time_on_trt, 
+                `Sedia LAg Odn screen`, sedia_ODn, viral_load, n_visits) %>% #, inter_test_interval
+  arrange(subject_label_blinded, test_date)
 
+model_cephia_2 <- nlme::lme(fixed = log10(viral_load) ~ sedia_ODn, #+ bs(time_on_trt, 3)
+                           random = ~ 1 | subject_label_blinded,
+                           data = data_generated %>%
+                             filter(time_on_trt>=0),
+                           na.action = na.exclude, control = lmeControl(opt = "optim")
+)
+summary(model_cephia_2)
 
-dev.off()
-
-# # after_ART <- final_dataset %>%
-# #   mutate(subject_label_blinded = as.character(subject_label_blinded)) %>%
-# #   filter(time_on_trt>=0) %>%
-# #   dplyr::select(subject_label_blinded, logvl, time_on_trt) %>%
-# #   left_join(pred_vl %>% 
-# #               mutate(subject_label_blinded = as.character(subject_label_blinded)) %>% 
-# #               select(subject_label_blinded, predict.subject_label_blinded), by = 'subject_label_blinded')
-# ## Exponential model
-# library(minpack.lm)
-# nls.fit <- function(lambda) {
-#   modle1 <- nlsLM(logvl ~ exp(-lambda * time_on_trt),
-#                   data = final_dataset %>%
-#                     filter(time_on_trt>=0),
-#                   start = list(lambda = lambda)
-#   ) # , verbose = FALSE)
-#   return(modle1)
-# }
-# lambda <- summary(nls.fit(0))
+model_cephia_2_suppressed <- nlme::lme(fixed = log10(viral_load) ~ sedia_ODn, #+ bs(time_on_trt, 3)
+                                       random = ~ 1 | subject_label_blinded,
+                                       data = data_generated_with_suppression %>%
+                                         filter(time_on_trt>=0),
+                                       na.action = na.exclude, control = lmeControl(opt = "optim")
+)
+summary(model_cephia_2_suppressed)
 
 after_ART <- final_dataset %>%
   mutate(subject_label_blinded = as.character(subject_label_blinded)) %>%
@@ -269,7 +259,6 @@ model3 <- nlme::lme(fixed = logvl ~ hiv_antibody_value, #+ bs(time_on_trt, 3)
                     na.action = na.exclude, control = lmeControl(opt = "optim")
 )
 summary(model3)
-
 
 data_without_sedia <- cbind((final_dataset %>%
   mutate(id = as.character(subject_label_blinded)) %>%
