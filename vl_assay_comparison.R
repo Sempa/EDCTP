0a3a283515b6b5b0ca0fd51055306c184b17c260
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2022-05-17 17:18:19 +0200

    Updated vl_assay_comparison file
    generating slopes for each individual depending on whether they are suppressed or not

--- vl_assay_comparison.R
+++ vl_assay_comparison.R
@@ -147,7 +147,7 @@ model_values_stradling_ARTstart <- nlme::lme(fixed = logvl ~ hiv_antibody_value,
                     na.action = na.exclude, control = lmeControl(opt = "optim")
 )
 summary(model_values_stradling_ARTstart)
-
+#############################################################################################################
 # studying the trajectory using CEPHIA samples that don't have pre 
 sedia_generic <- read_csv("data/20180410-EP-LAgSedia-Generic.csv") %>%
   filter(visit_hivstatus == "P") %>%
@@ -161,42 +161,22 @@ data_generated <- sedia_generic %>%
   arrange(subject_label_blinded, test_date) %>%
   filter(!is.na(sedia_ODn)) %>%
   distinct(subject_label_blinded, test_date, .keep_all = T) %>%
-  group_by(subject_label_blinded) %>%
-  mutate(inter_test_interval = c(30, diff(test_date))) %>%
-  filter(inter_test_interval>=30) %>% #removing visits that are less than two months apart. CEPHIA has some visits that are even one day apart
   filter(!is.na(viral_load)) %>%
+  group_by(subject_label_blinded) %>%
+  # mutate(inter_test_interval = c(30, diff(test_date))) %>%
+  # filter(inter_test_interval>=30) %>% #removing visits that are less than two months apart. CEPHIA has some visits that are even one day apart
+  mutate(mean_vl = mean(viral_load)) %>%
+  mutate(suprressed_throughout_followup = ifelse(mean_vl<=40, 1,0)) %>%
   mutate(visits = 1:length(subject_label_blinded)) %>%
   mutate(n_visits = max(visits)) %>%
   filter(n_visits>1) %>%
   ungroup() %>%
   mutate(time_on_trt = as.numeric(test_date - art_initiation_date),
          `Sedia LAg Odn screen` = sedia_ODn,
-         vl_detectable = as.factor(ifelse((viral_load) <=400, 0, ifelse(viral_load >400, 1, NA)))) %>%
+         vl_detectable = (ifelse((viral_load) <=40, 0, ifelse(viral_load >40, 1, NA)))
+         ) %>%
   dplyr::select(subject_label_blinded, test_date, art_initiation_date, time_on_trt, 
-                `Sedia LAg Odn screen`, sedia_ODn, viral_load, n_visits, vl_detectable) %>% #, inter_test_interval
-  arrange(subject_label_blinded, test_date)
-
-data_generated_with_suppression <- sedia_generic %>%
-  select(subject_label_blinded, test_date, art_initiation_date, aids_diagnosis_date, 
-         art_interruption_date, art_resumption_date, treatment_naive, 
-         on_treatment, first_treatment, sedia_ODn, viral_load) %>%
-  filter(!is.na(art_initiation_date)) %>%
-  arrange(subject_label_blinded, test_date) %>%
-  filter(!is.na(sedia_ODn)) %>%
-  filter(viral_load <=400) %>%
-  distinct(subject_label_blinded, test_date, .keep_all = T) %>%
-  group_by(subject_label_blinded) %>%
-  mutate(inter_test_interval = c(30, diff(test_date))) %>%
-  filter(inter_test_interval>=30) %>% #removing visits that are less than two months apart. CEPHIA has some visits that are even one day apart
-  filter(!is.na(viral_load)) %>%
-  mutate(visits = 1:length(subject_label_blinded)) %>%
-  mutate(n_visits = max(visits)) %>%
-  filter(n_visits>1) %>%
-  ungroup() %>%
-  mutate(time_on_trt = as.numeric(test_date - art_initiation_date),
-         `Sedia LAg Odn screen` = sedia_ODn) %>%
-  dplyr::select(subject_label_blinded, test_date, art_initiation_date, time_on_trt, 
-                `Sedia LAg Odn screen`, sedia_ODn, viral_load, n_visits) %>% #, inter_test_interval
+                `Sedia LAg Odn screen`, sedia_ODn, viral_load, n_visits, suprressed_throughout_followup, vl_detectable) %>% #, vl_detectable, inter_test_interval
   arrange(subject_label_blinded, test_date)
 
 model_cephia_2 <- nlme::lme(fixed = log10(viral_load) ~ sedia_ODn, #+ bs(time_on_trt, 3)
@@ -207,12 +187,93 @@ model_cephia_2 <- nlme::lme(fixed = log10(viral_load) ~ sedia_ODn, #+ bs(time_on
 )
 summary(model_cephia_2)
 
-model_cephia_2_suppressed <- nlme::lme(fixed = log10(viral_load) ~ vl_detectable:sedia_ODn, #+ bs(time_on_trt, 3)
-                                       random = ~ 1 | subject_label_blinded,
-                                       data = data_generated,
-                                       na.action = na.exclude, control = lmeControl(opt = "optim")
-)
-summary(model_cephia_2_suppressed)
+linear_reg_function <- function(dat, type) {
+  browser()
+  if (type == 1) {
+    if (dat$suprressed_throughout_followup == 0) {
+      model <- glm(
+        formula = log10(viral_load) ~ sedia_ODn,
+        family = gaussian,
+        data = dat
+      )
+    } else if (dat$suprressed_throughout_followup == 1) {
+      model <- glm(
+        formula = sedia_ODn ~ time_on_trt,
+        family = gaussian,
+        data = dat
+      )
+    }
+    return(cbind(
+      id = unique(dat$subject_label_blinded),
+      intercept = model$coefficients[[1]],
+      slope = model$coefficients[[2]],
+      suppressed = unique(dat$suprressed_throughout_followup)
+    ))
+  } else if (type == 2) {
+    if (dat$vl_detectable == 1) {
+      model <- glm(
+        formula = log10(viral_load) ~ sedia_ODn,
+        family = gaussian,
+        data = dat
+      )
+    } else if (dat$vl_detectable == 0) {
+      model <- glm(
+        formula = sedia_ODn ~ time_on_trt,
+        family = gaussian,
+        data = dat
+      )
+    }
+    return(cbind(
+      id = unique(dat$subject_label_blinded),
+      intercept = model$coefficients[[1]],
+      slope = model$coefficients[[2]],
+      suppressed = unique(dat$vl_detectable)
+    ))
+  }
+}
+#####dealing with unsuppressed visits
+model_data <- data.frame(id=NA, intercept = NA, slope = NA, suppressed=NA)
+counter <- 0
+unsuppressed_visit <- data_generated %>%
+  filter(vl_detectable==1)
+for (i in 1:length(unique(unsuppressed_visit$subject_label_blinded))) {
+  counter <- counter + 1
+  model_data[counter,] <- linear_reg_function(dat = subset(unsuppressed_visit, unsuppressed_visit$subject_label_blinded== unique(unsuppressed_visit$subject_label_blinded)[i]),
+                                              type = 2)
+}
+mean(model_data$slope, na.rm = T)
+summary(lm(slope~1, data = model_data))
+
+#####dealing with suppressed visits
+model_data_supp <- data.frame(id=NA, intercept = NA, slope = NA, suppressed=NA)
+counter <- 0
+suppressed_visit <- data_generated %>%
+  filter(vl_detectable==0)
+for (i in 1:length(unique(suppressed_visit$subject_label_blinded))) {
+  counter <- counter + 1
+  model_data_supp[counter,] <- linear_reg_function(dat = subset(suppressed_visit, suppressed_visit$subject_label_blinded== unique(suppressed_visit$subject_label_blinded)[i]),
+                                              type = 2)
+}
+
+mean(model_data_supp$slope, na.rm = T)
+summary(lm(slope~1, data = model_data_supp))
+
+model_data_supp%>%
+  ggplot(aes(x=slope, type=suppressed)) +
+  geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
+  scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  theme_ipsum() +
+  labs(fill="")
+cols <- c("#F76D5E", "#FFFFBF")#, "#72D8FF"
+v=rbind(model_data, model_data_supp) %>%
+  filter(!is.na(slope)) %>%
+  # filter(slope>-10) %>%
+  # filter(slope<5) %>%
+  ggplot(aes(x = slope, colour = as.factor(suppressed))) +
+    geom_density(lwd = 1.2, linetype = 1) + 
+  scale_color_manual(values = cols)
+v
+
 
 after_ART <- final_dataset %>%
   mutate(subject_label_blinded = as.character(subject_label_blinded)) %>%
