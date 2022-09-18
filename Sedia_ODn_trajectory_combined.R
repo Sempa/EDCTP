0b83bd5eb653e292c69cab2ca8b1c1d8841647b2
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2022-09-18 23:49:18 +0200

    generated slope for patients with visits straddling ART start

--- Sedia_ODn_trajectory_combined.R
+++ Sedia_ODn_trajectory_combined.R
@@ -13,6 +13,29 @@ library(distributions3)
 #############################################
 #' Estimating Sedia LAg Measurement noise
 #############################################
+final_dataset <- read_csv("Sempa_final_pull_with_results.csv") %>%
+  mutate(vl = ifelse(`Viral Load at Draw` == "<40", "40", ifelse(`Viral Load at Draw` == "NULL", "", `Viral Load at Draw`))) %>%
+  mutate(
+    logvl = log(as.numeric(vl), 10),
+    time_on_trt = -1 * `days since tx start`,
+    sedia_ODn = `Sedia LAg Odn screen`,
+    RaceEthnicity = `Race/Ethnicity`,
+    viral_load = as.numeric(vl),
+    test_date = visit_date,
+    days_since_eddi = `Days from EDDI to draw`
+  ) %>%
+  arrange(subject_label_blinded, time_on_trt) %>%
+  group_by(subject_label_blinded) %>%
+  mutate(visits = 1:length(subject_label_blinded),
+         unsupressed_visits = ifelse(viral_load >999, 1,0)) %>%
+  mutate(visits = ifelse(unsupressed_visits==1, visits, NA),
+    baseline_visit = ifelse(unsupressed_visits==1, max(visits, na.rm = T), 0)) %>%
+  mutate(baseline_visit = max(baseline_visit),
+         visits = 1:length(subject_label_blinded)) %>%
+  filter(visits >=baseline_visit) %>%
+  filter(Group == 'early suppressions') %>%
+  select(subject_label_blinded, days_since_eddi, test_date, sedia_ODn, viral_load) 
+
 sedia_generic <- read_csv("data/20180410-EP-LAgSedia-Generic.csv") %>%
   mutate(sedia_ODn = `result...15`)
 
@@ -239,6 +262,198 @@ for (i in c(100, 400, 1000)) {
   results <- rbind(results, x)
 }
 write.csv(results, "output_table/results_suppressed.csv") # results
+
+################################################################
+#' Analysis among early VL suppressed test straddling ART start
+################################################################
+
+slopes_for_suppressed_straddling_ARTstart <- function(ODn_vl_data, threshold) {
+  # browser()
+  dir.create(paste("strad_ARTstart_supp_", threshold, Sys.Date(), sep = "_"))
+  folder_name <- paste("strad_ARTstart_supp_", threshold, Sys.Date(), sep = "_")
+  data_generated <- ODn_vl_data# %>%
+    # select(
+    #   subject_label_blinded, days_since_eddi, test_date, sedia_ODn, viral_load # , art_initiation_date, aids_diagnosis_date,
+    #   # art_interruption_date, art_resumption_date, treatment_naive,
+    #   # on_treatment, first_treatment
+    # ) %>%
+    # arrange(subject_label_blinded, test_date) %>%
+    # # distinct(subject_label_blinded, test_date, .keep_all = T) %>%
+    # filter(!is.na(viral_load)) %>%
+    # group_by(subject_label_blinded) %>%
+    # mutate(flagvl = ifelse(viral_load <= threshold, 0, 1)) %>%
+    # mutate(suprressed_throughout_followup = ifelse(mean(flagvl) == 0, 1, 0)) %>%
+    # mutate(visits = 1:length(subject_label_blinded)) %>%
+    # mutate(n_visits = max(visits)) %>%
+    # filter(n_visits > 1) %>%
+    # ungroup()
+  
+  jpeg(paste(folder_name, "/", "Figure1", threshold, ".jpeg", sep = ""), units = "in", width = 8, height = 6, res = 300)
+  
+  print(
+    ggplot(
+      data = data_generated %>%
+        # filter(suprressed_throughout_followup == 1) %>%
+        group_by(subject_label_blinded),
+      aes(x = days_since_eddi, y = sedia_ODn)
+    ) + # as.factor(subject_label_blinded)
+      geom_line(aes(color = as.factor(subject_label_blinded)), size = 1) + # aes(color = subject_label_blinded, linetype = Group), group = Group
+      theme(
+        text = element_text(size = 20),
+        plot.title = element_text(hjust = 0.5),
+        axis.line = element_line(colour = "black"),
+        axis.text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), "null"),
+        legend.position = "none"
+      ) +
+      ylab("Sedia LAg ODn") +
+      xlab("Time since infection (Days)") +
+      ggtitle(paste("Sedia ODn vs EDDI_", threshold))
+  )
+  dev.off()
+  
+  model_data <- data.frame(
+    id = NA, interceptlink_identity = NA, slope_link_identity = NA,
+    intercept_link_log = NA, slope_link_log = NA
+  )
+  # browser()
+  set.seed(11)
+  dat <- data_generated %>%
+    # filter(suprressed_throughout_followup == 1) %>%
+    mutate(
+      sedia_ODn_with_noise = sedia_ODn + rnorm(n = length(subject_label_blinded), mean = 0, sd = noise$coefficients[1, 1] + noise$coefficients[2, 1] * sedia_ODn),
+      sedia_ODn_with_noise = ifelse(sedia_ODn_with_noise < 0, 0, ifelse(viral_load>999, sedia_ODn, sedia_ODn_with_noise)),
+      sedia_ODn_with_noise = ifelse(is.na(sedia_ODn_with_noise), sedia_ODn, sedia_ODn_with_noise)
+    ) %>%
+    filter(!is.na(days_since_eddi)) %>%
+    select(subject_label_blinded, test_date, days_since_eddi, sedia_ODn, sedia_ODn_with_noise, viral_load)
+  counter <- 0
+  for (i in 1:length(unique(dat$subject_label_blinded))) {
+    counter <- counter + 1
+    # print(unique(dat$subject_label_blinded)[i])
+    model_data[counter, ] <- ODn_regression_function_supressed_straddling_ARTstart(
+      dat = subset(dat, dat$subject_label_blinded == unique(dat$subject_label_blinded)[i])
+    )
+    # print(i)
+  }
+  # summary(model_data$slope_link_identity, na.rm = T)
+  # browser()
+  set.seed(11)
+  x <- rnorm(5e3, mean = mean(model_data$slope_link_identity, na.rm = T), sd = sd(model_data$slope_link_identity))
+  dx <- density(x)
+  jpeg(paste(folder_name, "/", "Figure_link_identity", threshold, ".jpeg", sep = ""), units = "in", width = 8, height = 6, res = 300)
+  
+  hist(model_data$slope_link_identity, breaks = 30, freq = FALSE, main = paste("Mean slope link=identity_", threshold), col = 'black', cex.lab = 1.5, cex.axis = 1.5)
+  lines(dx, lwd = 3, col = "red")
+  
+  # print(
+  # model_data %>%
+  #   ggplot(aes(x = slope_link_identity)) +
+  #   geom_histogram(color = "#e9ecef", alpha = 0.6, bins = 30, position = "identity") +
+  #   scale_fill_manual(values = c("#69b3a2", "#404080")) +
+  #   labs(fill = "") +
+  #   geom_density(
+  #     lwd = 1.2,
+  #     linetype = 1,
+  #     colour = 2
+  #   ) +
+  #   theme(
+  #     text = element_text(size = 20),
+  #     plot.title = element_text(hjust = 0.5),
+  #     axis.line = element_line(colour = "black"),
+  #     axis.text = element_text(size = 18),
+  #     axis.title = element_text(size = 18),
+  #     panel.background = element_blank(),
+  #     panel.border = element_blank(),
+  #     plot.margin = unit(c(0, 0, 0, 0), "null"),
+  #     legend.position = "none"
+  #   ) +
+  #   ggtitle(paste("histogram mean slope link=identity_", threshold))
+  
+  # )
+  dev.off()
+  # summary(model_data$slope_link_log, na.rm = T)
+  set.seed(11)
+  x <- rnorm(5e3, mean = mean(model_data$slope_link_log, na.rm = T), sd = sd(model_data$slope_link_log))
+  dx <- density(x)
+  
+  jpeg(paste(folder_name, "/", "Figure_link_log", threshold, ".jpeg", sep = ""), units = "in", width = 8, height = 6, res = 300)
+  
+  hist(model_data$slope_link_log, breaks = 30, freq = FALSE, main = paste("Mean slope link=log_", threshold), col = 'black', cex.lab = 1.5, cex.axis = 1.5)
+  lines(dx, lwd = 3, col = "red")
+  
+  # print(
+  #   model_data %>%
+  #     ggplot(aes(x = slope_link_log)) +
+  #     geom_histogram(color = "#e9ecef", alpha = 0.6, bins = 30, position = "identity") +
+  #     scale_fill_manual(values = c("#69b3a2", "#404080")) +
+  #     labs(fill = "") +
+  #     geom_density(
+  #       lwd = 1.2,
+  #       linetype = 1,
+  #       colour = 2
+  #     ) +
+  #     theme(
+  #       text = element_text(size = 20),
+  #       plot.title = element_text(hjust = 0.5),
+  #       axis.line = element_line(colour = "black"),
+  #       axis.text = element_text(size = 18),
+  #       axis.title = element_text(size = 18),
+  #       panel.background = element_blank(),
+  #       panel.border = element_blank(),
+  #       plot.margin = unit(c(0, 0, 0, 0), "null"),
+  #       legend.position = "none"
+  #     ) +
+  #     ggtitle(paste("histogram mean slope link=log_", threshold))
+  # )
+  dev.off()
+  
+  return(
+    cbind(
+      # suppression = threshold,
+      slope_mean_link_identity = mean(model_data$slope_link_identity),
+      pvalue_link_identity_slope_diff_zero = t.test((model_data$slope_link_identity))$p.value,
+      slope_mean_link_log = mean(model_data$slope_link_log),
+      pvalue_link_log_slope_diff_zero = t.test((model_data$slope_link_log))$p.value
+    )
+  )
+}
+
+ODn_regression_function_supressed_straddling_ARTstart <- function(dat) {
+  # browser()
+  model1 <- glm(
+    formula = sedia_ODn_with_noise ~ days_since_eddi,
+    family = gaussian(link = "identity"),
+    data = dat
+  )
+  
+  model2 <- glm(
+    formula = sedia_ODn_with_noise ~ days_since_eddi,
+    family = gaussian(link = "log"),
+    data = dat,
+    start = c(model1$coefficients[[1]], model1$coefficients[[2]])
+  )
+  
+  return(cbind(
+    id = unique(dat$subject_label_blinded),
+    intercept_link_identity = model1$coefficients[[1]],
+    slope_link_identity = model1$coefficients[[2]],
+    intercept_link_log = model2$coefficients[[1]],
+    slope_link_log = model2$coefficients[[2]]
+  ))
+}
+
+
+results <- c()
+# for (i in c(100, 400, 1000)) {
+  x <- slopes_for_suppressed_straddling_ARTstart(ODn_vl_data = final_dataset, threshold = 'ES')
+  results <- rbind(results, x)
+# }
+write.csv(results, "output_table/results_suppressed_straddling_ARTstart.csv") # results
+
 
 ################################################################################
 #' Describe Sedia LAg value slopes for records with last vl<1000 copies/ml to
