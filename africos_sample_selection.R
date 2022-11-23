bb7ade8b365c9c01d774cc2ee8a2b9ad6393adcc
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2022-11-23 21:34:22 +0200

    Update africos_sample_selection.R

--- africos_sample_selection.R
+++ africos_sample_selection.R
@@ -21,23 +21,24 @@ final_dataset <- pt_dataset %>%
          ) %>%
   mutate(min_flag_visit = min(flag_visit, na.rm = T),
          visit_after_ARTstart = ifelse(`STUDY VISIT (NUM)` > min_flag_visit, 1, 0)) %>%
-  # mutate(flag_suppressed_visit = ifelse(visit_after_ARTstart==1))
+  mutate(duration_on_ART = max(`Duration of started ART (years)`, na.rm = T)) %>%
   ungroup() %>%
   mutate(stradlling_art_start = ifelse(`NUMERIC DATE FOR art_sdt` > first_visit_date, T, F), #166 with ART start after first VL visit
-         `missing vl` = ifelse(!is.na(vl), F, T) # 236 missing viral loads But 38 visits have plasma vl available
+         `missing vl` = ifelse(!is.na(vl), F, T), # 236 missing viral loads But 38 visits have plasma vl available
+         `morethan 2yrs` = ifelse(duration_on_ART >=2, 1, 0) ## All have been followed up for at least 2 years
          ) %>% 
   filter(!is.na(vl)) %>%
   mutate(detectable_vl = ifelse((visit_after_ARTstart == 1 & `VL Copies/mL` >1000), 1, 0)) %>%
   group_by(`SUBJECT ID (CHAR)`) %>%
   mutate(ever_unsuppressed_after_artstart = max(detectable_vl, na.rm = T)) #%>% #28 people with detectable VL after ART start
-  # mutate(ever_unsuppressed_in_follo_wp = ifelse(`SUBJECT ID (CHAR)` %in%))
+  mutate(duration_on_ART)
 m=final_dataset%>%dplyr::select(`SUBJECT ID (CHAR)`, `PlACD Vol`, `PlACD  Vials` , vl, `missing vl`) %>% mutate(`missing plasma` = ifelse((`PlACD Vol`!='N/A' | `PlACD  Vials` != 'N/A') & is.na(vl), 1,0))
 table(m$`missing plasma`)
 x=final_dataset%>%
    filter(ever_unsuppressed_after_artstart == 1) #%>%
-## selecting patients who have been followed up for at least 2yrs
-  # mutate(country = grepl('A', `SUBJECT ID (CHAR)`))
 length(unique((final_dataset%>% mutate(country = grepl('A', `SUBJECT ID (CHAR)`)) %>% filter(country ==T & ever_unsuppressed_after_artstart==1))$`SUBJECT ID (CHAR)`))
 x_data <- pt_dataset %>%
-  filter(`SUBJECT ID (CHAR)` %in% unique((final_dataset%>% mutate(country = grepl('A', `SUBJECT ID (CHAR)`)) %>% filter(ever_unsuppressed_after_artstart==1))$`SUBJECT ID (CHAR)`))
+  filter(`SUBJECT ID (CHAR)` %in% unique((final_dataset%>% 
+                                            mutate(country = grepl('A', `SUBJECT ID (CHAR)`)) %>% 
+                                            filter(ever_unsuppressed_after_artstart==1))$`SUBJECT ID (CHAR)`))
 write.csv(x_data, 'data/ever_unsuppressed.csv')
