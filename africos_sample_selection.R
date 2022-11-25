cc702bbd57120e2f2e4ba2148ff2311d7146dfe1
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2022-11-25 17:13:29 +0200

    selecting samples
    Merged dataset for available samples with the main dataset. there are 7 repeated visits with same PT ID and date but differing viral load results.

--- africos_sample_selection.R
+++ africos_sample_selection.R
@@ -9,7 +9,14 @@ library(splines)
 library(cowplot)
 library(distributions3)
 
-pt_dataset <- read_excel("data/rv329_sacema_12sep22_Plasma Inventory[3].xlsx", sheet = "Request")
+pt_dataset <- read_excel("data/rv329_sacema_12sep22_Plasma Inventory[3].xlsx", sheet = "Request")[,-c(19:21)]
+pt_samples <- read_excel("data/rv329_sacema_12sep22_Plasma Inventory[3].xlsx", sheet = "Sheet1")[-c(1:3),] %>%
+  arrange(`Study Number`) %>%
+  mutate(Uganda = grepl('A', `Study Number`),
+         samples_exist = 1
+         ) %>% #select samples from Uganda only for logistical purposes
+  filter(Uganda == T) %>%
+  dplyr::select(`Study Number`, Plasma_acd_volume, plasma_edta_volume, Plasma_acd_vailcount, plasma_edta_vailcount, samples_exist)
 
 final_dataset <- pt_dataset %>%
   mutate(Uganda = grepl('A', `SUBJECT ID (CHAR)`))%>% #select samples from Uganda only for logistical purposes
@@ -21,17 +28,22 @@ final_dataset <- pt_dataset %>%
          ) %>%
   mutate(min_flag_visit = min(flag_visit, na.rm = T),
          visit_after_ARTstart = ifelse(`STUDY VISIT (NUM)` > min_flag_visit, 1, 0)) %>%
-  mutate(duration_on_ART = max(`Duration of started ART (years)`, na.rm = T)) %>%
+  mutate(duration_on_ART = max(`Duration of started ART (years)`, na.rm = T),
+         visits_before_art = ifelse(art_start>`Visit Date`, 1,0),
+         ) %>%
   ungroup() %>%
-  mutate(stradlling_art_start = ifelse(`NUMERIC DATE FOR art_sdt` > first_visit_date, T, F), #166 with ART start after first VL visit
+  mutate(stradlling_art_start = ifelse(`NUMERIC DATE FOR art_sdt` >= first_visit_date, T, F), #166 with ART start after first VL visit
          `missing vl` = ifelse(!is.na(vl), F, T), # 236 missing viral loads But 38 visits have plasma vl available
          `morethan 2yrs` = ifelse(duration_on_ART >=2, 1, 0) ## All have been followed up for at least 2 years
          ) %>% 
-  filter(!is.na(vl)) %>%
+  # filter(!is.na(vl)) %>%
   mutate(detectable_vl = ifelse((visit_after_ARTstart == 1 & `VL Copies/mL` >1000), 1, 0)) %>%
   group_by(`SUBJECT ID (CHAR)`) %>%
-  mutate(ever_unsuppressed_after_artstart = max(detectable_vl, na.rm = T)) #%>% #28 people with detectable VL after ART start
-  mutate(duration_on_ART)
+  mutate(ever_unsuppressed_after_artstart = max(detectable_vl, na.rm = T),
+         `Study Number` = paste(`SUBJECT ID (CHAR)`, `STUDY VISIT (NUM)`, sep = '-')
+         ) %>% #28 people with detectable VL after ART start
+  right_join(pt_samples, by = 'Study Number')
+
 m=final_dataset%>%dplyr::select(`SUBJECT ID (CHAR)`, `PlACD Vol`, `PlACD  Vials` , vl, `missing vl`) %>% mutate(`missing plasma` = ifelse((`PlACD Vol`!='N/A' | `PlACD  Vials` != 'N/A') & is.na(vl), 1,0))
 table(m$`missing plasma`)
 x=final_dataset%>%
