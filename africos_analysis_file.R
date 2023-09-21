7bcabb241c284959c84f0c242db6488a90fc6849
Joseph B. Sempa <jbsempa@yahoo.co.uk>
2023-09-21 13:59:42 +0200

    combining AFRICOS data and labs

--- africos_analysis_file.R
+++ africos_analysis_file.R
@@ -14,11 +14,53 @@ africos_data <- readxl::read_excel("data/Sedia® HIV-1 LAg-Avidity EIA ODs resul
   mutate(id = paste(`Date Collected`, `Study Number`, sep = '_'),
          `ODs 1` = `ODs`,
          `ODs 2` = `...13`,
-         `ODs 3` = `...14`) %>%
-  dplyr::select(id, `Date Collected`, `Study Number`, `ODs 1`, `ODs 2`,`ODs 3`) %>%
-  arrange(`Study Number`, `Date Collected`)
+         `ODs 3` = `...14`,
+         QC_8 = QC...8,
+         QC_9 = 1:length(`Date Collected`)) %>%
+  dplyr::select(id, `Date Collected`, `Study Number`, QC_8, QC_9, Plates, `ODs 1`, `ODs 2`,`ODs 3`)# %>%
+  # arrange(QC_9)
 dt01 <- africos_data %>%
   pivot_longer(cols = c('ODs 1', 'ODs 2','ODs 3'),
                names_to = 'ODs',
                values_to = 'OD')
-# length(unique(dt01$`Study Number`))
+dt02 <- readxl::read_excel("data/AFRICOS_Sempa Data Pull_24Jul23.xlsx", 
+                           sheet = "Sheet1") %>%
+  mutate(id = paste(Date_Collected, subjid, sep = '_')) %>%
+  group_by(subjid) %>%
+  mutate(switch_date = as.Date(as.numeric(ifelse(`ARV Start Date` == '.', '', `ARV Start Date`)), 
+                             origin = "1900-01-01"),
+         art_start = as.Date(min(as.numeric(ifelse(`ARV Start Date` == '.', '', `ARV Start Date`)), na.rm = T), 
+                             origin = "1900-01-01")) %>%
+  ungroup() %>%
+  dplyr::select(id, subjid, Visit, Age, Sex, `Viral Load`, Date_Collected, switch_date, art_start)
+africos_calibratorData <- bind_rows(readxl::read_excel("data/Sedia® HIV-1 LAg-Avidity EIA ODs results 2 calibrator data.xlsx", 
+                                             sheet = "Sheet1") %>% 
+                                      mutate(sheet = 1),
+                                    readxl::read_excel("data/Sedia® HIV-1 LAg-Avidity EIA ODs results 2 calibrator data.xlsx", 
+                                               sheet = "Sheet2") %>% 
+                                      mutate(sheet = 2),
+                                    readxl::read_excel("data/Sedia® HIV-1 LAg-Avidity EIA ODs results 2 calibrator data.xlsx", 
+                                               sheet = "Sheet3") %>% 
+                                      mutate(sheet = 3),
+                                    readxl::read_excel("data/Sedia® HIV-1 LAg-Avidity EIA ODs results 2 calibrator data.xlsx", 
+                                               sheet = "Sheet4") %>% 
+                                      mutate(sheet = 4),
+                                    readxl::read_excel("data/Sedia® HIV-1 LAg-Avidity EIA ODs results 2 calibrator data.xlsx", 
+                                               sheet = "Sheet5") %>% 
+                                      mutate(sheet = 5)
+) %>%
+  filter(!(row == 'H' & column == 12))
+calibratorData_sample <- africos_calibratorData %>%
+  filter(calibrator_infor == 'sample')
+dt03 <- bind_cols(dt01, calibratorData_sample) %>%
+  mutate(diff = OD-as.numeric(value))
+  # full_join(dt02, by = 'id') %>%
+  # arrange(`Study Number`, `Date Collected`)
+dt04 <- africos_calibratorData %>%
+  filter(calibrator_infor != 'sample') %>%
+  group_by(Plate, calibrator_infor) %>%
+  summarise(median_OD = median(as.numeric(value))) %>%
+  ungroup() %>%
+  filter(calibrator_infor == 'CAL')
+dt05 <- dt03 %>%
+  left_join(dt04, by = 'Plate')
